b2e6bf0926f84c097da664047ce7d57349d54224
Adam Gillfillan <adamgillfillan@vikings.berry.edu>
2015-02-17 17:01:06 -0500

    refactored main utterance handling

--- client/app/diction/diction.directive.js
+++ client/app/diction/diction.directive.js
@@ -25,7 +25,7 @@ angular.module('cookingBuddy20App')
   //       });
   //   };
   // })
-  .directive('myDialog', ['recipeService', function (recipeService) {
+  .directive('myDialog', function (recipeService, $timeout) {
     return function(scope, element, attrs) {
         console.log("inside link");
         // window.speechSynthesis.speak(new SpeechSynthesisUtterance("To begin with your recipe, say, what is the first step?"));
@@ -62,12 +62,36 @@ angular.module('cookingBuddy20App')
             recognition.start();
         };
 
-        var changeStepStyling = function (step, color, size) {
+        var beginRecognition = function() {
+            recognizing = true;
+            recognition.start();
+        };
+
+        var changeStepStyling = function(step, color, size) {
             document.getElementById("steps_list"+step).style.color = color;
             document.getElementById("steps_list"+step).style.fontSize = size;
-        }
+        };
 
-        recognition.onresult = function(event) {
+        var handleUtterance = function() {
+            message = recipeService.currRecipe.steps[recipeService.currentStep];
+            recognizing = false;
+            recognition.stop();
+            recipeService.speakMessage(message);
+            // don't listen to CookingBuddy's utterance. temporary hack solution
+            $timeout(beginRecognition, 6000);
+        };
+
+        var handleWatchAndListen = function() {
+            scope.$watch(function(scope) { return recipeService.currentStep },
+                function(newValue, oldValue) {
+                    changeStepStyling(oldValue, "", "1em");
+                    changeStepStyling(newValue, "red", "2em");
+                }
+            );
+            scope.$digest();
+        };
+
+        var buildTranscript = function() {
             interim_transcript = '';
             results = '';
             final_transcript = '';
@@ -79,24 +103,28 @@ angular.module('cookingBuddy20App')
                 } else {
                     interim_transcript += event.results[i][0].transcript;
                 }
-            }
+            };
+        };
+
+        var handleMatch = function(regex, value, increment) {
+            if (final_transcript.match(regex)){
+                if (increment)
+                    recipeService.currentStep += 1;
+                else
+                    recipeService.currentStep = value;
+                handleUtterance();
+            };
+        };
+
+        recognition.onresult = function(event) {
+            buildTranscript();
             
             if (final_transcript != ""){
                 console.log(final_transcript);
-                if (final_transcript.match(re) || final_transcript.match(re_2)){
-                    recipeService.currentStep += 1;
-                    message = recipeService.currRecipe.steps[recipeService.currentStep];
-                    recipeService.speakMessage(message);
-                }
-                console.log(recipeService.currentStep);
-                scope.$watch(function(scope) { return recipeService.currentStep },
-                    function(newValue, oldValue) {
-                        changeStepStyling(oldValue, "", "1em");
-                        changeStepStyling(newValue, "red", "2em");
-                    }
-                );
-                scope.$digest();
+                handleMatch(/first/i, 0, false);
+                handleMatch(/next/i, 1, true);
+                handleWatchAndListen();
             };
         };
     };
-  }]);
+  });
