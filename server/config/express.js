bff7fe73834ab0b1179b7ef9ad3a1e7f4fd9a53d
Adam <adam.gillfillan@vikings.berry.edu>
2015-03-08 17:41:49 -0400

    added license and redirect for http to https

--- server/config/express.js
+++ server/config/express.js
@@ -22,14 +22,6 @@ var mongoose = require('mongoose');
 module.exports = function(app) {
   var env = app.get('env');
 
-  var forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    return next();
-  };
-
-
   app.set('views', config.root + '/server/views');
   app.set('view engine', 'jade');
   app.use(compression());
@@ -54,7 +46,6 @@ module.exports = function(app) {
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
-    app.use(forceSsl);
   }
 
   if ('development' === env || 'test' === env) {
--- server/routes.js
+++ server/routes.js
@@ -7,14 +7,33 @@
 var errors = require('./components/errors');
 
 module.exports = function(app) {
+  /* At the top, with other redirect methods before other routes */
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
+  app.get('*',function(req,res,next){
+    if(req.headers['x-forwarded-proto']!='https'){
+      //res.header("Access-Control-Allow-Origin", "*");
+      //res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      //console.log("Https redirect");
+      res.redirect(['https://', req.get('Host'), req.url].join(''));
+      //res.redirect('https://cookingbuddy.herokuapp.com'+req.url);
+    }
+
+      //res.redirect(['https://', req.get('Host'), req.url].join(''));
+    else
+      next(); /* Continue to other routes if we're not redirecting */
+  });
   // Insert routes below
   app.use('/api/recipes', require('./api/recipe'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
